8fed80461ab7bf1b29752873834a74f57a668733
alaanoureldin22 <alaa.n.eldin@gmail.com>
2021-01-02 23:18:59 +0200

    implement Login & regsiter components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import React, { Fragment, useEffect } from 'react';
 
 import './App.css';
+import { Landing } from './components/layout/Landing';
+import { Navbar } from './components/layout/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Register } from './components/auth/Register';
+import { Login } from './components/auth/Login';
 
-const App = () => {
-  return <div>Client</div>;
-};
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+);
 
 export default App;
